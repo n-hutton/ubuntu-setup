480b79f871ed4bfbbdf44c5537921e3cd0353716
nhutton <nathan.cameron.hutton@gmail.com>
2018-08-23 15:55:39 +0100

    update with newest config changes

---
+++ update_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp ~/.gitconfig ./config_files
+cp ~/.vimrc ./config_files
+cp ~/.vim/spell ./vim_files -r
+mkdir -p ./vim_files/bundle
+cp ~/.vim/bundle/vim-snippets ./vim_files/bundle -r
+rm ./vim_files/bundle/vim-snippets/.git -rf
